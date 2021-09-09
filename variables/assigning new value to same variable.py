67cab76f2076ab805315cf2d4a3d6f15bab87b9b
idha-dalal <85778406+idha-dalal@users.noreply.github.com>
2021-09-10 00:20:44 +0530

    assigning values to same named variables
    ways in which we can assign new values to same named variables

---
+++ variables/assigning new value to same variable.py
@@ -0,0 +1,15 @@
+mv_population=20
+mv_population=30
+print('new population of mv school(way 1) is:',mv_population)
+# the new value of mv_population will be taken up
+
+# also if we know the change in population we can write it as
+mv_population=20
+mv_population=mv_population+10
+print('new population of mv school(way 2) is:',mv_population)
+
+# for minimising our work 
+# mv_population=mv_population+10  this statement can be written as
+mv_population=20
+mv_population+=10
+print('new population of mv school(way 3) is:',mv_population)
